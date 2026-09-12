eb88314eabf4dc090d8aa9f8abc7bb5163416b38
googy <kok030224@gmail.com>
2026-09-13 02:59:50 +0900

    새 할일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,58 @@
-export default function Home() {
-  return <div>리액트 프로잭튼</div>;
-}
+"use client";
+
+import React, { useState } from "react";
+
+const NewTodoForm = ({ addTodo: _addTodo }) => {
+  const [newTodoTitle, setnewTodoTitle] = useState("");
+
+  const addTodo = () => {
+    if (newTodoTitle.trim().length == 0) return;
+
+    const id = 1;
+    const title = newTodoTitle.trim();
+
+    const newTodo = {
+      id,
+      title,
+    };
+
+    _addTodo(newTodo);
+    setnewTodoTitle("");
+  };
+
+  return (
+    <form className="flex gap-[5px]" onSubmit={(e) => e.preventDefault()}>
+      <input
+        className="input input-bordered"
+        type="text"
+        value={newTodoTitle}
+        placeholder="새 할일을 입력해주세요."
+        onChange={(e) => setnewTodoTitle(e.target.value)}
+      />
+      <button className="btn btn-primary" onClick={addTodo}>
+        할일추가
+      </button>
+    </form>
+  );
+};
+
+const TodoList = ({ todos }) => {
+  return <>{JSON.stringify(todos)}</>;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <>
+      <NewTodoForm addTodo={addTodo} />
+      <TodoList todos={todos} />
+    </>
+  );
+};
+
+export default App;
